4d89c97e1d7d94152c18c327d01d50729c9dc6ff
Mỹ Duyên <myduyenn218@gmail.com>
2020-06-22 10:27:06 +0700

    edit download file

--- DataWareHouse/src/main/java/myduyen/Download/App.java
+++ DataWareHouse/src/main/java/myduyen/Download/App.java
@@ -1,18 +1,69 @@
 package myduyen.Download;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import ExtractData.DBConnection;
+import ExtractData.ExtractData;
 
 /**
  * App
  */
 public class App {
-	public static void main(String[] args) {
-		SynologyNas nas = new SynologyNas("http://drive.ecepvn.org:5000/webapi", "guest_access", "123456");
 
-		ArrayList<RemoteFile> filePaths = nas.list("/ECEP/song.nguyen/DW_2020/data", 0, 0);
+	public App() throws ClassNotFoundException, SQLException {
+		ExtractData ex = new ExtractData();
+		String jdbcURL_1 = "jdbc:mysql://localhost/controldata?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		String userName_1 = "admin";
+		String password_1 = "admin";
+		Connection connectionDB = DBConnection.getConnection(jdbcURL_1, userName_1, password_1);
+
+		String sql = "Select * FROM downloadconfig WHERE id = 1";
+
+		PreparedStatement p = connectionDB.prepareStatement(sql);
+		ResultSet resultSet = p.executeQuery(sql);
+		ResultSetMetaData rsmd = resultSet.getMetaData();
+//		int columnsNumber = rsmd.getColumnCount();
+
+		String url, remoteFile, location, username, password;
+		resultSet.next(); // đọc dòng đầu tiên
+
+		url = resultSet.getString("url");
+		username = resultSet.getString("username");
+		password = resultSet.getString("password");
+		remoteFile = resultSet.getString("remoteFile");
+		location = resultSet.getString("location");
+
+		String inserLogs = "INSERT INTO logs (id_filename, myconfig_id,status,transform  ) VALUES (?,?,?,?)";
+		PreparedStatement pre = connectionDB.prepareStatement(inserLogs);
+		SynologyNas nas = new SynologyNas(url, username, password);
+		ArrayList<RemoteFile> filePaths = nas.list(remoteFile, 0, 0);
 		for (RemoteFile file : filePaths) {
-			System.out.printf("Name: %s, Path: %s, isDir: %s\n", file.getName(), file.getPath(), file.isDir());
+//			System.out.printf("Name: %s, Path: %s, isDir: %s\n", file.getName(), file.getPath(), file.isDir());
+			if (nas.download(remoteFile, location + file.getName())) {
+				pre.setString(1, file.getName());
+				pre.setInt(2, 1);
+				pre.setString(3, "OK");
+				pre.setString(4, "ReadyTransfrom");
+				pre.execute();
+			} else {
+				pre.setString(1, file.getName());
+				pre.setInt(2, 1);
+				pre.setString(3, "Fails");
+				pre.setString(4, "NotReadyTransfrom");
+				pre.execute();
+			}
 		}
-		nas.download("/ECEP/song.nguyen/DW_2020/data", "/home/myduyen/data.zip");
+
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		new App();
+
 	}
 }
